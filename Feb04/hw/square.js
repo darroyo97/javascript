a5880f33da922c024e04b5f4fad969e9ff70ab77
Daniela <daniela.arroyo27@gmail.com>
2020-02-04 14:48:31 -0600

    square

---
+++ Feb04/hw/square.js
@@ -0,0 +1,13 @@
+var dimension = 5;
+var edge = '*';
+var inside = '*';
+var printLine;
+
+for (var i = 1; i <= dimension; i++) {
+    if (i === 1 || i === dimension) {
+        printline = Array(dimension + 1).join(edge);
+    } else {
+        printline = edge + Array(dimension - 1).join(inside) + edge;
+    }
+    console.log(printline);
+}

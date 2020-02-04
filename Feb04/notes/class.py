f40cca2c430299f71cff1aec46a365c7e693aa13
Daniela <daniela.arroyo27@gmail.com>
2020-02-04 11:19:53 -0600

    comment errors

---
+++ Feb04/notes/class.py
@@ -0,0 +1,24 @@
+school = "digital crafts"
+
+
+# def myFunction()
+
+
+# print("hello")
+
+
+print("hello world")
+
+myString = """
+multi line
+yeyeyey
+"""
+
+newString = "hello" + " " + "world"
+
+myBool = True
+
+python_we_write_snake = 0
+
+thisString = "daniela"
+# l = length(thisString)

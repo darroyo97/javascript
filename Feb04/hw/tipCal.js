2b7c2b6ee87a2cc0b93822b407503ecc4010d204
Daniela <daniela.arroyo27@gmail.com>
2020-02-04 13:48:29 -0600

    tip calc complete

---
+++ Feb04/hw/tipCal.js
@@ -0,0 +1,18 @@
+function tipCalc(amount, serviceLevel) {
+    amount = amount
+    if (serviceLevel == "bad") {
+        tip = amount * 0.20
+        total = tip + amount
+        return total
+    } else if (serviceLevel == "fair") {
+        tip = amount * 0.15
+        total = tip + amount
+        return total
+    } else if (serviceLevel == "good") {
+        tip = amount * 0.10
+        total = tip + amount
+        return total
+    }
+}
+
+console.log(tipCalc(87.34, "good"))

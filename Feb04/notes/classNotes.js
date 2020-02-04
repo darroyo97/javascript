fe1cfe0aa42ede1065141346aaac364c89b2bada
Daniela <daniela.arroyo27@gmail.com>
2020-02-04 10:46:05 -0600

    seeing diffrence between python n js

--- Feb04/notes/classNotes.js
+++ Feb04/notes/classNotes.js
@@ -1,1 +1,9 @@
 console.log("goodbye dani")
+
+var school = "digital crafts";
+// indentation doesnt matter like python
+function myFunction() {
+    console.log("hello")
+}
+
+console.log("hello world")

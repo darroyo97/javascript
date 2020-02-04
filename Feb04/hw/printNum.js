e2b6c7f9efb919905dcd96203c1f554ad8356a5a
Daniela <daniela.arroyo27@gmail.com>
2020-02-04 13:56:25 -0600

    num count for and while

---
+++ Feb04/hw/printNum.js
@@ -0,0 +1,21 @@
+// function printNumbers() {
+//     var count = 0
+//     while (count < 10) {
+//         count += 1;
+//         console.log(count)
+
+//     }
+// }
+
+// printNumbers()
+
+
+function printNumbers() {
+    for (var count = 0;
+        count < 10;
+        count++) {
+        console.log(count)
+    }
+}
+
+printNumbers()

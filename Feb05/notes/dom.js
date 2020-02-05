c339f5e593137e1f27a88011cf1d5445c96c18ee
Daniela <daniela.arroyo27@gmail.com>
2020-02-05 11:32:05 -0600

    query selectors

--- Feb05/notes/dom.js
+++ Feb05/notes/dom.js
@@ -7,22 +7,39 @@
 
 //
 
-var imgArray = document.getElementsByTagName('img')
-// console.log(imgArray[0])
-console.log(imgArray[1])
-var src = imgArray[1].getAttribute("src")
-console.log(src)
+// var imgArray = document.getElementsByTagName('img')
+// // console.log(imgArray[0])
+// console.log(imgArray[1])
+// var src = imgArray[1].getAttribute("src")
+// console.log(src)
 // imgArray[0].setAttribute("src", "https://townsquare.media/site/519/files/2019/04/GettyImages-480615728.jpg?w=980&q=75")
 
 //
 
-var divArray = document.getElementsByClassName('hello')
-console.log(divArray)
-console.log(divArray.length)
-var firstDiv = divArray[0]
-console.log(firstDiv)
-divArray[0].textContent = "good morning"
-divArray[1].innerHTML = "<b>zip ti py do da</b>"
+// var divArray = document.getElementsByClassName('hello')
+// console.log(divArray)
+// console.log(divArray.length)
+// var firstDiv = divArray[0]
+// console.log(firstDiv)
+// divArray[0].textContent = "good morning"
+// divArray[1].innerHTML = "<b>zip ti py do da</b>"
 
 //
+
+// works just like css: examples
+// var body = document.querySelector('.hello')
+// var body = document.querySelector('#myFirstId')
+
+
+// is powerful tool where you can use multiple:example
+// var body = document.querySelector('div p img')
+// console.log(body)
+// prints only first one
+
+// var div = document.querySelectorAll('div')
+// console.log(div)
+
+
+//
+
 

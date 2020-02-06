0cfec57a9ee6f3bb98f71f5f6677ee6f4a47f8d6
Daniela <daniela.arroyo27@gmail.com>
2020-02-06 11:47:27 -0600

    const, let, var explaind

--- Feb06/notes/functions/class.js
+++ Feb06/notes/functions/class.js
@@ -1,2 +1,9 @@
-var add = x + 1;
-console.log(add)
+// var add = x + 1;
+// console.log(add)
+var index = 0;
+
+for (let index = 0; index <= 10; index++) {
+    console.log(index)
+}
+
+console.log("final value of index:", index)

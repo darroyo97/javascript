4c4e6f695d35d93c882effddc5d573628d463433
Daniela <daniela.arroyo27@gmail.com>
2020-02-06 15:55:08 -0600

    cities 2

---
+++ Feb06/hw/medium/cities2.js
@@ -0,0 +1,10 @@
+var cities = [
+    { name: 'Los Angeles', temperature: 60.0 },
+    { name: 'Houston', temperature: 100.0 },
+    { name: 'New York', temperature: 40.0 },
+    { name: 'Miami', temperature: 90.0 },
+    { name: 'Seattle', temperature: 30.0 },
+]
+cities.forEach(function (city) {
+    console.log(city.name)
+})

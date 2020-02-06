03de184e8d35917744df89d391d25428e23fc9ae
Daniela <daniela.arroyo27@gmail.com>
2020-02-06 11:29:57 -0600

    scope

--- Feb06/notes/functions/classNotes.js
+++ Feb06/notes/functions/classNotes.js
@@ -11,12 +11,55 @@
 // console.log(c)
 //pass by value
 
-var myArray = [1, 2, 3, 4, 5]
-var myArrayCopy = myArray
-console.log(myArray)
-console.log(myArrayCopy)
-myArrayCopy[0] = 33
-// var myArrayCopy = [33, 4, 5, 6, 7]
-console.log(myArray)
-console.log(myArrayCopy)
-//pass by reference
+// var myArray = [1, 2, 3, 4, 5]
+// var myArrayCopy = myArray
+// console.log(myArray)
+// console.log(myArrayCopy)
+// myArrayCopy[0] = 33
+// // var myArrayCopy = [33, 4, 5, 6, 7]
+// console.log(myArray)
+// console.log(myArrayCopy)
+// //pass by reference
+
+//scoping
+// function testFun() {
+//     var name = "dani";
+// }
+
+// console.log(name) //not defined error 
+
+
+var a = 9
+var b = 10
+
+// function add(a, b) {
+//     return a + b
+// }
+
+// console.log(a)
+//defiend outside
+
+// function add(a, b) {
+//     a = 1
+//     console.log(a)
+// }
+
+
+var x = 1;
+if (x === 1) {
+    let x = 2;
+    console.log("inside of", x);
+    // expected output: 2
+}
+
+function test() {
+    var x = 3
+    return x
+}
+
+// x = test()
+test()
+console.log("outside of", x);
+// expected output: 1
+
+//let releases value only uses once

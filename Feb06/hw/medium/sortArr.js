477bf30a7f27ce82789688fac9416b0d1e488318
Daniela <daniela.arroyo27@gmail.com>
2020-02-06 16:11:41 -0600

    sort

---
+++ Feb06/hw/medium/sortArr.js
@@ -0,0 +1,11 @@
+// Given an array of strings such the array of names given in
+// the previous problem, sort them by alphabetically order.
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+console.log(fruits)
+
+// var sortFruits = fruits.map(function (name) {
+//     return fruits.sort()
+// })
+// console.log(sortFruits)

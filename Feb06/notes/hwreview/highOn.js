941bc63c475d6fa49d77a1524118c8659d3380d1
Daniela <daniela.arroyo27@gmail.com>
2020-02-06 10:40:10 -0600

    added set att in function

--- Feb06/notes/hwreview/highOn.js
+++ Feb06/notes/hwreview/highOn.js
@@ -1,5 +1,5 @@
 var container = document.querySelector('#container')
-function createHTMLElement(tagName, parentNode, innerText, setClassName) {
+function createHTMLElement(tagName, parentNode, innerText, setClassName, link) {
     var tag = document.createElement(tagName);
     if (innerText.length > 0) {
         tag.textContent = innerText;
@@ -7,24 +7,20 @@ function createHTMLElement(tagName, parentNode, innerText, setClassName) {
     if (setClassName.length > 0) {
         tag.className = setClassName;
     }
+    if (link.length > 0) {
+        tag.setAttribute("href", link)
+    }
     parentNode.append(tag);
     return tag
 }
-var link1 = createHTMLElement("a", container, "High On Coding", "nav-item nav-link active")
-// idk how to add set Att to function
-link1.setAttribute("href", "#");
-var link2 = createHTMLElement("a", container, "Home", "nav-item nav-link active small pl-5")
-link2.setAttribute("href", "#");
-var link3 = createHTMLElement("a", container, "Categories", "nav-item nav-link active small pl-5")
-link3.setAttribute("href", "#");
-
-var headerDiv = createHTMLElement("div", container, "", "nav navbar-nav h3")
-
+var link1 = createHTMLElement("a", container, "High On Coding", "nav-item nav-link active", "#")
+var link2 = createHTMLElement("a", container, "Home", "nav-item nav-link active small pl-5", "#")
+var link3 = createHTMLElement("a", container, "Categories", "nav-item nav-link active small pl-5", "#")
+var headerDiv = createHTMLElement("div", container, "", "nav navbar-nav h3", "")
 headerDiv.append(link1);
 headerDiv.append(link2);
 headerDiv.append(link3);
-
-var headerNav = createHTMLElement("nav", container, "", "navbar navbar-expand navbar-dark p-4")
+var headerNav = createHTMLElement("nav", container, "", "navbar navbar-expand navbar-dark p-4", "")
 headerNav.style = "background-color: rgb(82, 167, 250); ";
 headerNav.append(headerDiv);
 container.append(headerNav);

943caef25b464494141ebb7f58d539cdbe14815c
Daniela <daniela.arroyo27@gmail.com>
2020-02-06 11:11:52 -0600

    pass by value, pass by ref

--- Feb06/notes/functions/classNotes.js
+++ Feb06/notes/functions/classNotes.js
@@ -1,5 +1,22 @@
-(function () {
-    var x = "Hello!!";
-    console.log(x)  // I will invoke myself
-})();
+// (function () {
+//     var x = "Hello!!";
+//     console.log(x)  // I will invoke myself
+// })();
 
+// var a = 4
+// var b = 5
+// var c = a
+// var c = 88
+// console.log(a)
+// console.log(c)
+//pass by value
+
+var myArray = [1, 2, 3, 4, 5]
+var myArrayCopy = myArray
+console.log(myArray)
+console.log(myArrayCopy)
+myArrayCopy[0] = 33
+// var myArrayCopy = [33, 4, 5, 6, 7]
+console.log(myArray)
+console.log(myArrayCopy)
+//pass by reference

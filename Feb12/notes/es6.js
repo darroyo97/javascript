b5731361c1965b923dbba1f360e9d1bc108059dc
Daniela <daniela.arroyo27@gmail.com>
2020-02-12 09:56:06 -0600

    array placement

--- Feb12/notes/es6.js
+++ Feb12/notes/es6.js
@@ -7,6 +7,18 @@
 
 
 // we can put js in curly braces
-let a = 3
-let b = 4
-console.log(`The total is: ${a + b}`)
+// let a = 3
+// let b = 4
+// console.log(`The total is: ${a + b}`)
+
+// let myArray = [4, 5, 6, 7, 2]
+
+let [a, b, c, d, e] = [4, 5, 6, 7, 2]
+
+console.log(a);
+console.log(b)
+
+// let a = myArray[0]; //4
+// let b = myArray[2] //6
+// let c = myArray[3] // 7
+

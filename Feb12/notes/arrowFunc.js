a74c1c576cac7cf22b60888171c7a37822af2ecb
Daniela <daniela.arroyo27@gmail.com>
2020-02-12 10:07:18 -0600

    arrow functions

---
+++ Feb12/notes/arrowFunc.js
@@ -0,0 +1,13 @@
+// ARROW FUNCTIONS
+
+// function helloWorld() {
+//     console.log('Hello World')
+// }
+
+let helloWorld = () => {
+    console.log('Hello World')
+}
+helloWorld()
+
+// they are the same ^^^ how to re write function in to arrow function
+

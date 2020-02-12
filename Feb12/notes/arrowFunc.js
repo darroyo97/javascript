3fed1e1097bf490abba231292c668dcaa9bf0fc1
Daniela <daniela.arroyo27@gmail.com>
2020-02-12 11:14:22 -0600

    rest params

--- Feb12/notes/arrowFunc.js
+++ Feb12/notes/arrowFunc.js
@@ -56,25 +56,78 @@
 
 //////
 
-let obj = {
-    x: 5,
-    y: 20,
-    z: 3
-}
+// let obj = {
+//     x: 5,
+//     y: 20,
+//     z: 3
+// }
 //create func called mult
 // let mult = ((object) => {
 //     return object.x * object.y * object.z
 // })
 // console.log(mult(obj));
 
-let mult = ((object) => {
-    let { x, y, z } = object
-    return x * y * z
-})
-console.log(mult(obj));
+// let mult = ((object) => {
+//     let { x, y, z } = object
+//     return x * y * z
+// })
+// console.log(mult(obj));
 
 // let { x, y, z } = obj
 // console.log(x)
 // console.log(y)
 // console.log(z)
 
+// let div = document.getElementById('myId')
+
+// div.addEventListener('click', function (e) {
+//     ///sotmhing something
+// })
+// // div.addEventListener('click', (e) => {
+// //     ///something something
+// // })
+
+//EXAMPLE ****************^^^^ OF SAME REWRITTEN TO ARROW
+
+// let addTwoNums = (a, b) => {
+//     a = a || 0;
+//     b = b || 0;
+//     return a + b
+// }
+// //if no a is 0 if no v is 0 
+// // let result = addTwoNums(2)
+// // we passed a but no b so is ts 2 + 0
+// let result = addTwoNums(2, 3)
+// // now it changes both
+
+// console.log(result)
+
+// let multNums = () => {
+//     console.log(arguments.length)
+// }
+// multNums(4, 6, 5, 7)
+
+//es5 rest paramaters
+// function logArguments() {
+//     for (var i = 0; i < arguments.length; i++) {
+//         console.log(arguments[i]);
+//     }
+// }
+// logArguments(3, 4, 5, 6, 7)
+///
+///es6 rest parameters
+// function logArguments(...args) {
+//     for (let arg of args) {
+//         console.log(arg);
+//     }
+// }
+// logArguments(3, 4, 5, 6, 7)
+// let multNums = (...args) => {
+//     let m = 1;
+//     for (let i of args) {
+//         m = m * i;
+//     }
+//     console.log(m)
+// }
+
+// multNums(2, 3, 4)

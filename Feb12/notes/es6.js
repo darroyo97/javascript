e2b0f98797a45bdf43f0d0a958afb3a86f759ece
Daniela <daniela.arroyo27@gmail.com>
2020-02-12 09:52:53 -0600

    curly braces in clg string

--- Feb12/notes/es6.js
+++ Feb12/notes/es6.js
@@ -1,6 +1,12 @@
 // console.log(x);
 // // ReferenceError: x is not defined
 // let x = 'hi';
-let today = new Date();
-console.log(today.toString())
-console.log(`Today's date is ${today}`)
+// let today = new Date();
+// console.log(today.toString())
+// console.log(`Today's date is ${today.toLocaleTimeString()}`)
+
+
+// we can put js in curly braces
+let a = 3
+let b = 4
+console.log(`The total is: ${a + b}`)

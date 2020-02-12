bbd2e4e6f4c475a75fada650f3869e7e1aa54642
Daniela <daniela.arroyo27@gmail.com>
2020-02-12 10:00:15 -0600

    reassign variables

--- Feb12/notes/es6.js
+++ Feb12/notes/es6.js
@@ -13,12 +13,30 @@
 
 // let myArray = [4, 5, 6, 7, 2]
 
-let [a, b, c, d, e] = [4, 5, 6, 7, 2]
+// let [a, b, c, d, e] = [4, 5, 6, 7, 2]
 
-console.log(a);
-console.log(b)
+// console.log(a);
+// console.log(b)
 
 // let a = myArray[0]; //4
 // let b = myArray[2] //6
 // let c = myArray[3] // 7
+
+// let myArray = [4, 'hi', 'chicken', 69, 'baby yoda']
+// let [a, b, c, d, e] = myArray
+// console.log(a)
+// console.log(e)
+
+//assign values ^^
+
+// let myArray = [4, 'hi', 'chicken', 69, 'baby yoda']
+// let [a, b, c] = myArray
+// console.log(a)
+// console.log(e)
+// //will be undefined need to assign
+
+let luke = { occupation: 'jedi', father: 'anakin' }
+//dot . and [] are the same
+let job = luke[occupation]
+let father = luke.father
 

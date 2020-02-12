39b5307165f025d89995aadfefaa157c17b0999a
Daniela <daniela.arroyo27@gmail.com>
2020-02-12 13:45:12 -0600

    pratacing regular exp

---
+++ Feb12/notes/reg.js
@@ -0,0 +1,10 @@
+// let regex = new RegExp('digital crafts')
+// let regex2 = /digital crafts/
+// let someString = "fhkshfksh digital crafts snfsfsdf sfhskfjs "
+// let result = regex.test(someString);
+// console.log(result)
+
+let pattern = /Daniela Arroyo/
+let string = "do you know her? yeah Daniella Arroyo"
+let result = pattern.test(string)
+console.log(result)

44b0d67f5ea636bce39253abf42c2f05fbca9021
Daniela <daniela.arroyo27@gmail.com>
2020-02-12 10:47:45 -0600

    using destructioning and arrow func

--- Feb12/notes/arrowFunc.js
+++ Feb12/notes/arrowFunc.js
@@ -29,15 +29,52 @@
 
 //default parameters
 
-let nums = [1, 2, 3, 4, 5]
-// let newArray = nums.map(function (num) {
-//     ///
-// })
+// let nums = [1, 2, 3, 4, 5]
+// // let newArray = nums.map(function (num) {
+// //     ///
+// // })
 
-let newArray = nums.map((num) => {
-    return num * 2
-})
-console.log(newArray)
+// let newArray = nums.map((num) => {
+//     return num * 2
+// })
+// console.log(newArray)
 
 //same thing but has arrow function//   EXAMPLE*****^^^
+
+///
+
+// let arr = [1, -1, 2, -2, 3, -3]
+// let newArray = arr.filter((element) => {
+//     return element > 0
+// })
+// console.log(newArray)
+// var newArr = arr.filter(function (element) {
+//     return element > 0;
+// })
+
+// console.log(newArr)
+
+//////
+
+let obj = {
+    x: 5,
+    y: 20,
+    z: 3
+}
+//create func called mult
+// let mult = ((object) => {
+//     return object.x * object.y * object.z
+// })
+// console.log(mult(obj));
+
+let mult = ((object) => {
+    let { x, y, z } = obj
+    return x * y * z
+})
+console.log(mult(obj));
+
+// let { x, y, z } = obj
+// console.log(x)
+// console.log(y)
+// console.log(z)
 

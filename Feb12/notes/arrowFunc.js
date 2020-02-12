f6bdb9a5392240c747c80b155eae46542a66974c
Daniela <daniela.arroyo27@gmail.com>
2020-02-12 10:34:31 -0600

    arrow in callback func

--- Feb12/notes/arrowFunc.js
+++ Feb12/notes/arrowFunc.js
@@ -9,9 +9,35 @@
 // }
 // helloWorld()
 
-// they are the same ^^^ how to re write function in to arrow function
+// they are the same ^^^ how to re write function in to arrow function *** EXAMPLE***
 
 //short cut let met
 // let methodName = (params) => {
 
 // }
+
+// setTimeout(function () {
+//     console.log('hello wolrd')
+// }, 2000)
+// console.log('first')
+
+//hello world prints until 2 seconds
+
+// function getType() {
+//     console.log('Type is: ' + this.type)
+// }
+
+//default parameters
+
+let nums = [1, 2, 3, 4, 5]
+// let newArray = nums.map(function (num) {
+//     ///
+// })
+
+let newArray = nums.map((num) => {
+    return num * 2
+})
+console.log(newArray)
+
+//same thing but has arrow function//   EXAMPLE*****^^^
+

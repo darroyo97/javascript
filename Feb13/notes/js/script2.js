c297406f12ad0fc8e42048da9ad4e6d0ed720e2f
Daniela <daniela.arroyo27@gmail.com>
2020-02-13 11:47:53 -0600

    notes on forms and examples

---
+++ Feb13/notes/js/script2.js
@@ -0,0 +1,9 @@
+$(() => {
+    $('#myForm').submit((e) => {
+        e.preventDefault()
+        alert('form was sumbited')
+    })
+})
+// let myFunction = () => {
+//     alert('form sumbitted')
+// }

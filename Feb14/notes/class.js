38c10859ee1dfd9d34ecc17fc26a3eab862ea91a
Daniela <daniela.arroyo27@gmail.com>
2020-02-14 13:43:48 -0600

    intro into advance objects

--- Feb14/notes/class.js
+++ Feb14/notes/class.js
@@ -1,8 +1,20 @@
-let Course = (title, instructor, campus) => {
+function Course(title, instructor, campus) {
     this.title = title;
     this.instructor = instructor;
     this.campus = campus;
 }
 let janCohort = new Course("Immersive bootcamp", "Veronica", "Houston")
 let janCohort2 = new Course("Immersive bootcamp", "Sean", "Atlanta")
-let novCohort = new Course("Immersive bootcamp", "Azam", "Houston")
+let novCohort = new Course("Flex bootcamp", "Azam", "Houston")
+
+// console.log(janCohort.instructor)
+delete janCohort.title
+console.log(janCohort)
+
+// let classes = []
+// classes.push(janCohort)
+// or we can 
+// let classes = [
+//     new Course("Immersive Bootcamp", "Veronica", "Houston"),
+//     new Course("Flex Bootcamp", "Azam", "Atlanta")
+// ]

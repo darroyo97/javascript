c4857c35bedf68853d770ce97f02c47bdcb3c06f
Daniela <daniela.arroyo27@gmail.com>
2020-02-14 13:36:00 -0600

    first commit of the day

---
+++ Feb14/notes/class.js
@@ -0,0 +1,8 @@
+let Course = (title, instructor, campus) => {
+    this.title = title;
+    this.instructor = instructor;
+    this.campus = campus;
+}
+let janCohort = new Course("Immersive bootcamp", "Veronica", "Houston")
+let janCohort2 = new Course("Immersive bootcamp", "Sean", "Atlanta")
+let novCohort = new Course("Immersive bootcamp", "Azam", "Houston")

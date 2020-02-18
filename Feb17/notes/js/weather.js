6323e41f14a71a22b3bb9a64c6cc70a204d6bb94
Daniela <daniela.arroyo27@gmail.com>
2020-02-17 19:05:31 -0600

    logic working

--- Feb17/notes/js/weather.js
+++ Feb17/notes/js/weather.js
@@ -1,9 +1,34 @@
 //
-function wawa() {
-    var variable = document.getElementById('input_id').value;
-    document.getElementById('alert').innerHTML = 'The user input is: ' + variable;
+function myFunc() {
+    let variable = document.getElementById('input_id').value;
+    let userCity = document.getElementById('nameCity')
+    userCity.innerHTML = 'The city you picked is : ' + variable;
+    console.log(variable)
+    var userUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + variable + `&APPID=9cd5ceea03f0af9dfe6cda9fc0684c66`
+    // console.log(userUrl)
+    fetch(userUrl)
+        .then((response) => {
+            return response.json();
+        })
+        .then((weatherObj) => {
+            // console.log(weatherObj.main.temp)
+            let celsius = weatherObj.main.temp - 273.15
+            // console.log(celsius)
+            let degCRound = Math.floor(celsius)
+            // console.log(degCRound)
+            let degF = degCRound * 1.8 + 32;
+            // console.log(degF)
+            let degFRound = Math.floor(degF)
+            console.log(degFRound)
+            let answer = document.querySelector('#tempResult');
+            answer.innerHTML = degFRound.toString()
+
+        })
+
 }
-let url = `https://api.openweathermap.org/data/2.5/weather?q=Houston&APPID=9cd5ceea03f0af9dfe6cda9fc0684c66`
+
+// console.log(variable)
+// let url = `https://api.openweathermap.org/data/2.5/weather?q=Houston&APPID=9cd5ceea03f0af9dfe6cda9fc0684c66`
 // fetch(url)
 //     .then((response) => {
 //         return response.json();

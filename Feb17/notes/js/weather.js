f960da13a848445c80e99b880edffb81075cc48d
Daniela <daniela.arroyo27@gmail.com>
2020-02-17 18:48:23 -0600

    user inpput

--- Feb17/notes/js/weather.js
+++ Feb17/notes/js/weather.js
@@ -1,19 +1,24 @@
+//
+function wawa() {
+    var variable = document.getElementById('input_id').value;
+    document.getElementById('alert').innerHTML = 'The user input is: ' + variable;
+}
 let url = `https://api.openweathermap.org/data/2.5/weather?q=Houston&APPID=9cd5ceea03f0af9dfe6cda9fc0684c66`
-fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((weatherObj) => {
-        // console.log(weatherObj.main.temp)
-        let celsius = weatherObj.main.temp - 273.15
-        // console.log(celsius)
-        let degCRound = Math.floor(celsius)
-        // console.log(degCRound)
-        let degF = degCRound * 1.8 + 32;
-        // console.log(degF)
-        let degFRound = Math.floor(degF)
-        console.log(degFRound)
-        let body = document.querySelector('body');
-        body.innerHTML = degFRound.toString()
+// fetch(url)
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((weatherObj) => {
+//         // console.log(weatherObj.main.temp)
+//         let celsius = weatherObj.main.temp - 273.15
+//         // console.log(celsius)
+//         let degCRound = Math.floor(celsius)
+//         // console.log(degCRound)
+//         let degF = degCRound * 1.8 + 32;
+//         // console.log(degF)
+//         let degFRound = Math.floor(degF)
+//         console.log(degFRound)
+//         let body = document.querySelector('body');
+//         body.innerHTML = degFRound.toString()
 
-    })
+//     })
